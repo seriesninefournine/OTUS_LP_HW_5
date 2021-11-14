0ce4992edd8f0cf297652857fb04a4ff116e4620
seriesninefournine <seriesninefournine@gmail.com>
2021-11-15 02:47:44 +0500

    now work NFSv3+UDP

--- startup_script_client.sh
+++ startup_script_client.sh
@@ -2,8 +2,12 @@
 
 #Настраиваем autofs для автоматического подключения к ресурсам
 dnf -y install autofs
-echo "/mnt  /etc/auto.nfs4 --browse" >> /etc/auto.master
-echo "nfs_share4   -rw        10.0.0.10:/" > /etc/auto.nfs4
+#echo "/mnt  /etc/auto.nfs4 --browse" >> /etc/auto.master
+#echo "nfs_share4   -rw,udp        10.0.0.10:/" > /etc/auto.nfs4
+
+echo "/mnt  /etc/auto.nfs3 --browse" >> /etc/auto.master
+echo "nfs_share3   -rw,udp        10.0.0.10:/var/nfs_share/" > /etc/auto.nfs3
 systemctl restart autofs
 
 #mount -t nfs4  10.0.0.10:/ /mnt/nfs_share4
+#mount -o vers=3,proto=udp  10.0.0.10:/var/nfs_share/ /mnt/nfs_share3
--- startup_script_server.sh
+++ startup_script_server.sh
@@ -7,20 +7,31 @@ chmod 777 /var/nfs_share/upload/
 #Настраиваем NSFv4 сервер
 dnf -y install nfs-utils
 
-systemctl mask --now rpc-statd.service rpcbind.service rpcbind.socket
+#systemctl mask --now rpc-statd.service rpcbind.service rpcbind.socket
 sed -i -e 's/# vers2=n/vers2=n/g' /etc/nfs.conf
-sed -i -e 's/# vers3=y/vers3=n/g' /etc/nfs.conf
-echo 'SECURE_NFS="yes"' >> /etc/nfs.conf
+sed -i -e 's/# vers3=y/vers3=y/g' /etc/nfs.conf
+sed -i -e '/# tcp=y/ a\udp=y' /etc/nfs.conf
+
+# tcp=y
+# vers2=n
+# vers3=y
+# vers4=n
+# vers4.0=n
+# vers4.1=n
+# vers4.2=n
+
+#sed -i -e 's/# vers3=y/vers3=n/g' /etc/nfs.conf
+#echo 'SECURE_NFS="yes"' >> /etc/nfs.conf
 systemctl enable --now nfs-server
 
 #Публикуем ресурсы
-echo "/var/nfs_share/       *(sync,rw,fsid=0,insecure,no_subtree_check)  
-/var/nfs_share/upload/ *(sync,rw,nohide,insecure,no_subtree_check)" > /etc/exports
-
+#echo "/var/nfs_share/       *(sync,rw,fsid=0,insecure,no_subtree_check)  
+#/var/nfs_share/upload/ *(sync,rw,nohide,insecure,no_subtree_check)" > /etc/exports
+echo "/var/nfs_share/ *(sync,rw,insecure,no_subtree_check)" > /etc/exports
 exportfs -ra
 
 #Настриваем firewalld
 systemctl enable --now firewalld
 firewall-cmd --permanent --add-service={nfs,nfs3,ssh,mountd}
-#firewall-cmd --add-service={rpc-bind,nfs,nfs3,ssh}
-firewall-cmd --reload
+firewall-cmd --permanent --add-port=111/udp --add-port=2049/udp
+systemctl restart firewalld
